c300b4716ee59b2366fc37d4d593bc5b09f0d213
AxelGeorg <axelgeorg16@gmail.com>
2024-12-12 14:58:42 -0300

    Testes

---
+++ 19 - Testes Automatizados/1 - Introdução/enderecos/enderecos_test.go
@@ -0,0 +1,50 @@
+package enderecos_test
+
+import (
+	. "introducao-testes/enderecos"
+	"testing"
+)
+
+type cenarioDeTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
+//go test
+//go test --cover
+//go test --coverprofile cobertura.txt
+//go tool cover --func=cobertura.txt
+//go tool cover --html=cobertura.txt
+
+func TestTipoDeEndereco(t *testing.T) {
+	t.Parallel()
+
+	cenariosDeTeste := []cenarioDeTeste{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia dos Imigrantes", "Rodovia"},
+		{"Praça das Rosas", "Tipo Inválido"},
+		{"Estrada Qualquer", "Estrada"},
+		{"RUA DOS BOBOS", "Rua"},
+		{"AVENIDA REBOUÇAS", "Avenida"},
+		{"", "Tipo Inválido"},
+	}
+
+	for _, cenario := range cenariosDeTeste {
+		retornoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+		if retornoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s",
+				retornoRecebido,
+				cenario.retornoEsperado,
+			)
+		}
+	}
+}
+
+func TestQualquer(t *testing.T) {
+	t.Parallel()
+
+	if 1 > 2 {
+		t.Errorf("Teste quebrou!")
+	}
+}
